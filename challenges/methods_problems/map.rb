602ba80096b2aa0ca88d5bf5b8b64e0d30cd4ac8
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-06-16 14:23:49 +0500

    Reduce cost of nil objects

--- challenges/methods_problems/map.rb
+++ challenges/methods_problems/map.rb
@@ -4,11 +4,12 @@ array = [2, -2, 6, 4, -1, 3, -4, 1]
 positive_array = []
 negative_array = []
 
-positive_array = array.map{|element| element >= 0 ? element : nil}
-positive_array = positive_array - [nil]
+positive_array = array.map{|element| element >= 0 ? element : next}
+#positive_array.flatten!
+#positive_array = positive_array - [nil]
 
-negative_array = array.map{|element| element < 0 ? element : nil}
-negative_array -= [nil]
+negative_array = array.map{|element| element < 0 ? element : next}
+#negative_array -= [nil]
 
 print "Positive Array: ", positive_array, "\n"
 print "Negative Array: ", negative_array, "\n"
