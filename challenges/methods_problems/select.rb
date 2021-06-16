a0b3f5ee9b42468b96694afa77494fbebd7dda29
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-06-16 13:56:47 +0500

    Impelment Select

--- challenges/methods_problems/select.rb
+++ challenges/methods_problems/select.rb
@@ -1,4 +1,4 @@
-# find the odd numbers from array, if numbers are duplicate so remove it
+# Problem 1: find the odd numbers from array, if numbers are duplicate so remove it
 array = [1,5,3,1,9,5,7,6,2,7]
 
 print array.select{|element| element.odd?}.uniq
