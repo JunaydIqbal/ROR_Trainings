566fde59f4614e999bdbec908a605f7bc65d04cc
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-06-16 13:50:03 +0500

    Impelment delete if

--- challenges/methods_problems/delete_if.rb
+++ challenges/methods_problems/delete_if.rb
@@ -1,4 +1,5 @@
 #Problem: show the players scores of greater than and equal to 100
+# it updates the array with delete the element which condition execute
 
 players = {junaid: 89, moiz: 120, arslan: 144, salman: 99, hamza: 100}
 
