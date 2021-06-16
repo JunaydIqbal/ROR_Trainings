9ba6208cc80dd5e61c2c17b7cf7177f75581ce10
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-06-16 13:55:55 +0500

    Impelment reject

--- challenges/methods_problems/reject.rb
+++ challenges/methods_problems/reject.rb
@@ -1,4 +1,4 @@
-# find the even numbers from array, if numbers are duplicate so remove it
+# Problem 1: find the even numbers from array, if numbers are duplicate so remove it
 array = [1,5,3,1,9,5,7,6,2,7]
 
 print array.reject{|element| element.odd?}.uniq
