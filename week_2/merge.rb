e94ad074fa72259900cba77651e1ebdd0cae0b46
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-06-16 17:35:11 +0500

    merge problems

---
+++ week_2/merge.rb
@@ -0,0 +1,26 @@
+#Problem 1: 3 students in class so merge the assignenmts with total marks
+
+# std_1 = {ass1: 12, ass2: 42, ass3: 36, ass4: 41}
+# std_2 = {ass1: 10, ass2: 15, ass3: 39, ass4: 43}
+# std_3 = {ass1: 11, ass2: 32, ass3: 25, ass4: 34}
+
+# marks = std_1.merge(std_2, std_3){|key, prev_value, new_value| prev_value + new_value}
+
+# print marks
+
+
+
+
+#Problem 2
+# order = {
+#     id: 1001,
+#     OS: 'WINDOW'
+#    }
+   
+#    user = {
+#      name: 'Junaid',
+#      email: 'junaid@gmail.com' 
+#    }
+# newHash = order.merge(user)
+
+# print newHash
